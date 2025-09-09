6990f56047c1f3fb9d42209232d3d4b1caec806a
bixboy <pierrelamare62@gmail.com>
2025-09-09 10:26:16 +0200

    OUI OUI au pays des bites

---
+++ Assets/Scripts/DollyCam/Rail.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    public bool isLoop = true;
+
+    private float _length;
+
+    private List<Vector3> _nodes;
+
+    private void Start()
+    {
+        
+    }
+
+    float GetLength()
+    {
+        return _length;
+    }
+
+    Vector3 GetPosition(float distance)
+    {
+        if (_nodes.Count == 0)
+            return Vector3.zero;
+        
+        Vector3 firstNode = _nodes[0] 
+        return transform.position;
+    }
+}
